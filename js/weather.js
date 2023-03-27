110d52e931a664d953735b279c4509925120c270
Jaejin Kim <jaejink@uci.edu>
2023-03-26 18:57:58 -0700

    weather API implementation

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,12 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     console.log("You live in", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
+    fetch(url).then(response => response.json().then(data => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+            weatherContainer.innerText =`${data.weather[0].main} / ${data.main.temp}`;
+    }));
 }
 
 function onGeoError() {
